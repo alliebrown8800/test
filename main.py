2f3029bbbcf65399ad931e92bc7c113996debf63
alliebrown8800 <91629048+alliebrown8800@users.noreply.github.com>
2021-10-20 14:23:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python37all
+# Save file as /usr/lib/cgi-bin/test.py
+
+print('Content-type:text/html\n\n')
+print('<html><body>')
+print('<form action="\cgi-bin/test.py" method="POST"')
+print('<input type="submit" value="create a new page">')
+print('</form>')
+print('</body></html>')
